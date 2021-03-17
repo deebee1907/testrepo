d131667c16db99687873ba5ee679a0303366a4db
deebee1907 <80811147+deebee1907@users.noreply.github.com>
2021-03-17 12:46:20 +0530

    Create first_file.py
    Creating a new file

---
+++ first_file.py
@@ -0,0 +1,5 @@
+print("Hello there!")
+print("Gonna casually print the 9's time table...")
+for multiplier in range(1, 21):
+    print("9 x {} = {}".format(multiplier, 9*multiplier))
+print("All done!")
